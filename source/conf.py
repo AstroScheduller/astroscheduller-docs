7547f3234707cb2375997b1b954422b567861cfa
Wenky <771623548@qq.com>
2022-05-07 01:40:29 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -57,3 +57,8 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 source_suffix = ['.rst', '.md']
+
+# Html5 export theme
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
